10a6992f5329ebc32546c74c634fe7b85e18b54a
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-06-15 14:53:29 +0530

    Update Fibonacci File

--- Fibonacci_Series_Using_Recursion.py
+++ Fibonacci_Series_Using_Recursion.py
@@ -2,6 +2,7 @@ def fibonacci_recursive(n):
     if n <= 1:
         return n
     return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
-print("\n\nFibonacci series:",end=" ")
-for i in range(10):
+n = int(input("Enter Number: "))
+print("\nFibonacci series:",end=" ")
+for i in range(n):
     print(fibonacci_recursive(i), end=" ")
